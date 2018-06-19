6338b345bf41f99f04e9bd039e041d3530e0d62a
ggwozdz90 <40333300+ggwozdz90@users.noreply.github.com>
2018-06-19 06:56:04 +0200

    Connection string info

--- src/Tests/StorageClient.Azure.Test/Constants.cs
+++ src/Tests/StorageClient.Azure.Test/Constants.cs
@@ -3,7 +3,7 @@
     public static class Constants
     {
         public const string ConnectionString =
-            "DefaultEndpointsProtocol=https;AccountName=ospazurebackup;AccountKey=gTvHlTBfKqEiiq1Meu6hiIMTMhl5/03khlFdgHOx6oBxQlWTKno9ANM2HSjK0XhDLcjCPAd6eyH2HgIPNTqQNg==;EndpointSuffix=core.windows.net";
+            "Put here connection string";
 
         public const string AzureFileShareWithStructure = "ospbackup";
         public const string AzureFileShareWithBigFile = "bigfile";
